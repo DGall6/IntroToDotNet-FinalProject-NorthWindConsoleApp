bbcd53969500f5e2f7e5bcab629140c514a473fb
Donovan Gall <dgall6@my.wctc.edu>
2025-04-17 13:28:39 -0500

    [1.10] Display categories

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using NLog;
 using System.Linq;
 using NorthwindConsole.Model;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 string path = Directory.GetCurrentDirectory() + "//nlog.config";
 // create instance of Logger
 var logger = LogManager.Setup().LoadConfigurationFromFile(path).GetCurrentClassLogger();
@@ -19,6 +21,22 @@ do
   if (choice == "1")
   {
     // display categories
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile($"appsettings.json");
+
+    var config = configuration.Build();
+
+    var db = new DataContext();
+    var query = db.Categories.OrderBy(p => p.CategoryName);
+
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"{query.Count()} records returned");
+    Console.ForegroundColor = ConsoleColor.Magenta;
+    foreach (var item in query)
+    {
+      Console.WriteLine($"{item.CategoryName} - {item.Description}");
+    }
+    Console.ForegroundColor = ConsoleColor.White;
   }
   else if (choice == "2")
   {
